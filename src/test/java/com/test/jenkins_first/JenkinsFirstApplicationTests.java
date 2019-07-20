6e5aa68da12ece38a085254c022e804493f6dada
Anil  Meena <anil.meena@industrybuying.com>
2019-07-20 18:56:56 +0530

    first_commit

---
+++ src/test/java/com/test/jenkins_first/JenkinsFirstApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.jenkins_first;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsFirstApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
